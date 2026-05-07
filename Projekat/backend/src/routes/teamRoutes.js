4b6e167a87130032c5f2b081b1d7fb7272041bc7
ihindija1 <ihindija1@etf.unsa.ba>
2026-05-07 02:49:11 +0200

    Resolved all merge conflicts including teamRoutes

--- Projekat/backend/src/routes/teamRoutes.js
+++ Projekat/backend/src/routes/teamRoutes.js
@@ -1,46 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-
-const router = express.Router();
-
-const { authenticateToken } = require('../middleware/authMiddleware');
-const { requireRole } = require('../middleware/roleMiddleware');
-
-// GET svi timovi (javno)
-router.get('/', (req, res) => {
-  res.json({ poruka: 'Svi timovi' });
-});
-
-// CREATE tim
-router.post(
-  '/',
-  authenticateToken,
-  requireRole('ADMINISTRATOR', 'ORGANIZATOR'),
-  (req, res) => {
-    res.json({ poruka: 'Tim kreiran' });
-  }
-);
-
-// UPDATE tim
-router.put(
-  '/:id',
-  authenticateToken,
-  requireRole('ADMINISTRATOR', 'ORGANIZATOR'),
-  (req, res) => {
-    res.json({ poruka: 'Tim azuriran' });
-  }
-);
-
-// DELETE tim
-router.delete(
-  '/:id',
-  authenticateToken,
-  requireRole('ADMINISTRATOR', 'ORGANIZATOR'),
-  (req, res) => {
-    res.json({ poruka: 'Tim obrisan' });
-  }
-);
-=======
 const router = express.Router();
 const teamController = require('../controllers/teamController');
 
@@ -59,6 +17,5 @@ router.delete('/:id', authenticateToken, teamController.deleteTeam);
 router.post('/:id/players', authenticateToken, teamController.addPlayer);
 router.post('/:id/coaches', authenticateToken, teamController.addCoach);
 router.delete('/:id/players/:playerId', authenticateToken, teamController.removePlayer);
->>>>>>> origin/feature/PB-25-Teams-and-Sports-Management
 
 module.exports = router;
