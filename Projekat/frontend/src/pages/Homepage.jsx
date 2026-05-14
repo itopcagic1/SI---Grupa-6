8ffde550e8cc907300ba79de475ceacf2ee77c03
irma <itopcagic1@etf.unsa.ba>
2026-05-14 18:23:51 +0200

    deploy

--- Projekat/frontend/src/pages/Homepage.jsx
+++ Projekat/frontend/src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 
+const API_URL = 'https://si-grupa-6.onrender.com' || 'http://localhost:3000';
 // SVG Ikone za UI
 const CalendarIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
@@ -58,7 +59,7 @@ const Homepage = () => {
   useEffect(() => {
     const fetchHomepageData = async () => {
       try {
-        const response = await axios.get('https://sportmanager-frontend.onrender.com');
+        const response = await axios.get(`${API_URL}/api/homepage`);
         if (response.data && response.data.uspjeh) {
           setData(response.data.podaci);
         }
